9380393b3596a9b0598aad8a9d2e5831c9f805cc
Garrett Murphey <gmurphey@gmurphey.com>
2019-05-09 11:18:27 -0400

    Excluding helpers if ember-event-helpers addon is installed.
    This adds ember-cli-version-checker to detect the existence of ember-event-helpers. If found, the helpers packaged with this addon are excluded from the tree.

--- index.js
+++ index.js
@@ -1,5 +1,32 @@
 'use strict';
 
+const VersionChecker = require('ember-cli-version-checker');
+const Funnel = require('broccoli-funnel');
+
 module.exports = {
-  name: require('./package').name
+  name: require('./package').name,
+
+  init(...args) {
+    this._super.init && this._super.init.call(this, ...args);
+
+    const checker = new VersionChecker(this.project);
+    const eventHelpersCheck = checker.for('ember-event-helpers');
+
+    this.shouldShipHelpers = !eventHelpersCheck.exists();
+  },
+
+  treeForAddon(tree, ...args) {
+    let addonTree = tree;
+
+    if (!this.shouldShipHelpers) {
+      addonTree = new Funnel(tree, {
+        exclude: ['helpers']
+      });
+    }
+
+    return (
+      this._super.treeForAddon &&
+      this._super.treeForAddon.apply(this, [addonTree, ...args])
+    );
+  }
 };
